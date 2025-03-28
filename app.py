542a4c24591344b3aef88d957a1006efeedb3882
Parsa Sadeghi Asl <parsasadeghiasl@gmail.com>
2025-03-28 21:30:57 +0330

    implemented caching for images and videos

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, send_file, url_for
+from flask_caching import Cache
 import os
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
+cache.init_app(app) 
 app.config['DATA_FOLDER'] = 'media'
 
 # Allowed file extensions
@@ -13,21 +16,24 @@ def allowed_file(filename):
 @app.route('/')
 def index():
     files = []
-    if os.path.exists(app.config['DATA_FOLDER']):
-        # Get all files and sort them by type (images first, then videos)
-        all_files = []
-        for filename in os.listdir(app.config['DATA_FOLDER']):
-            if allowed_file(filename):
-                file_type = 'video' if filename.rsplit('.', 1)[1].lower() in {'mp4', 'webm', 'mov'} else 'image'
-                all_files.append({
-                    'name': filename,
-                    'type': file_type,
-                    'path': url_for('serve_file', filename=filename),
-                    'download_path': url_for('download_file', filename=filename)
-                })
+    files = cache.get('files')
+    if not files:
+        if os.path.exists(app.config['DATA_FOLDER']):
+            # Get all files and sort them by type (images first, then videos)
+            all_files = []
+            for filename in os.listdir(app.config['DATA_FOLDER']):
+                if allowed_file(filename):
+                    file_type = 'video' if filename.rsplit('.', 1)[1].lower() in {'mp4', 'webm', 'mov'} else 'image'
+                    all_files.append({
+                        'name': filename,
+                        'type': file_type,
+                        'path': url_for('serve_file', filename=filename),
+                        'download_path': url_for('download_file', filename=filename)
+                    })
         
         # Sort files: images first, then videos
         files = sorted(all_files, key=lambda x: (x['type'] == 'video', x['name']))
+        cache.set('files', files)
     
     return render_template('index.html', files=files)
 
